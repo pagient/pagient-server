f777956bb3301e910ef93653b04d174f21c703fe
David Schneiderbauer <dschneiderbauer@gmail.com>
2019-05-14 17:32:28 +0200

    fix pager validation

--- internal/model/pager.go
+++ internal/model/pager.go
@@ -4,7 +4,6 @@ import (
 	"regexp"
 
 	"github.com/go-ozzo/ozzo-validation"
-	"github.com/go-ozzo/ozzo-validation/is"
 	"github.com/pkg/errors"
 )
 
@@ -19,7 +18,7 @@ type Pager struct {
 func (pager *Pager) Validate() error {
 	if err := validation.ValidateStruct(pager,
 		validation.Field(&pager.Name, validation.Required, validation.Match(regexp.MustCompile("[[:print:]]+$"))),
-		validation.Field(&pager.EasyCallID, validation.Required, is.Digit),
+		validation.Field(&pager.EasyCallID, validation.Required),
 	); err != nil {
 		if e, ok := err.(validation.InternalError); ok {
 			return errors.Wrap(e, "internal validation error occurred")
